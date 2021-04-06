ab156ec33482b35e8450637480d5be3fe1a6dba1
Enver Aguinaga <enverdavid01@gmail.com>
2021-04-06 10:15:00 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,19 @@
+
+export const getGifs = async(category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=bDuAek0WgtdME85g8Jeumlmaun1lCbYd`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url,
+        }
+    })
+
+    return gifs;
+   
+
+}
